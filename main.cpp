a76582d279e507caadfb400f0ca5e8b45d9992f3
22015179 <161758456+22015179@users.noreply.github.com>
2024-04-18 20:22:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char gender, alphabet;
+    int money,age;
+    string name;
+
+    cout << "enter a your name";
+    cin >> name;
+
+    cout <<"enter your age";
+    cin >>age;
+
+    cout <<"enter your gender in M/f ";
+    cin >>gender;
+
+    if ( age<=35){
+            if(gender !=gender){
+                    if (money<=20000){
+
+
+
+                    cout<<"merry that person";
+
+
+
+            }
+            else{
+
+                cout<< "Leave him he's not ready to make family";
+
+
+            }
+
+             return 0;
+
+       }
+    }
+}
+
+
+
+
